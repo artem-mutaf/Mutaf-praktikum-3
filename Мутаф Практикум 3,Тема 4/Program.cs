76474cdfb83c969c0e4bb534368df891a9b1bc5f
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2023-10-09 12:30:48 +0300

    Add files via upload
    16 задание Практикум 3 Тема 4

---
+++ Мутаф Практикум 3,Тема 4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace программа_1
+{
+    
+    
+    
+
+      internal class Program
+        {
+
+          static void Main(string[] args)
+          {
+            
+            Console.Write("Введите первое слово - ");
+            string name1 = Console.ReadLine();
+            Console.Write("Введите второе слово - ");
+            string name2 = Console.ReadLine();
+            Console.Write("Введите третье слово - ");
+            string name3 = Console.ReadLine();
+            Console.Write("Введите четвертое слово - ");
+            string name4 = Console.ReadLine();
+            Console.Write("Введите пятое слово - ");
+            string name5 = Console.ReadLine();
+
+            Console.WriteLine(name1);
+            Console.Write(name2 + "\n");
+            Console.Write(name3 + "\n");
+            Console.Write(name4 + "\n");
+            Console.Write(name5);
+            Console.ReadKey();
+
+
+          }
+        }
+    
+}
